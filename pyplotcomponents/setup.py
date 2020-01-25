b18bdb20acc4f149cf583cf8b70f3fda5dbf58c7
Marko Budiselic <mbudiselicbuda@gmail.com>
2020-01-25 15:30:26 +0100

    Add pyplotcomponents setup.py script

---
+++ pyplotcomponents/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='pyplotcomponents',
+    version='0.1.0',
+    description='Python Plot Components',
+    author='Marko Budiselic',
+    author_email='mbudiselicbuda@gmail.com',
+    packages=['pyplotcomponents'],
+    install_requires=['matplotlib==3.1.2']
+)
